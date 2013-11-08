3f1288e54c979830400ac7d3a02f68dc7e19ffa0
DJJessie <groendj@gmail.com>
2013-11-08 12:58:27 +0100

    Change the computeImageDistance function

--- week1/week1.py
+++ week1/week1.py
@@ -47,14 +47,18 @@ def computeVectorDistance( vec1, vec2, dist_type ):
     return dist
     
 def computeImageDistances( images ):
-    # PRE [DO NOT TOUCH]
-    imdists = []
+    histo = []
+    for i in list(xrange(len(images))):
+        histo.append(extractColorHistogram((images[i])))
+        #print histo[2]
     
-    # WRITE YOUR CODE HERE
-    for i in range(0, 60):
+    # E.2 Compute distances between all images in the dataset
+    imdists = []
+    for i in range(0,60):
         imdists.append([])
-        for j in range(0, 60):
-            imdists[i].append(extractColorHistogram.histo(images))
+        for j in range (0,60):
+            imdists[i].append(computeVectorDistance(histo[i], histo[j], 'euclidean'))
+    
     
     # RETURN [DO NOT TOUCH]
     return imdists
